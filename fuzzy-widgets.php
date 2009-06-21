16bdc90381e57733165e7874262a53c0c8d13a27
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-06-21 12:04:05 +0000

    inline widgets enhancements
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/plugins/fuzzy-widgets/trunk@1800 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- fuzzy-widgets.php
+++ fuzzy-widgets.php
@@ -141,7 +141,15 @@ class fuzzy_widget extends WP_Widget {
 		extract($args, EXTR_SKIP);
 		extract($instance, EXTR_SKIP);
 		
-		if ( is_admin() || !in_array($type, array('pages', 'posts', 'links', 'comments', 'old_posts', 'updates')) )
+		if ( is_admin() ) {
+			echo $before_widget
+				. ( $title
+					? ( $before_title . $title . $after_title )
+					: ''
+					)
+				. $after_widget;
+			return;
+		} elseif ( !in_array($type, array('pages', 'posts', 'links', 'comments', 'old_posts', 'updates')) )
 			return;
 		
 		$cache_id = "$widget_id";
