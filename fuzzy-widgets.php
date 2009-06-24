e42e9f805b7b83f9318c81747c0cfe80b2a19a64
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-06-24 21:28:09 +0000

    add missing widget_title filters
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/plugins/fuzzy-widgets/trunk@1827 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- fuzzy-widgets.php
+++ fuzzy-widgets.php
@@ -144,7 +144,7 @@ class fuzzy_widget extends WP_Widget {
 		if ( is_admin() ) {
 			echo $before_widget
 				. ( $title
-					? ( $before_title . $title . $after_title )
+					? ( $before_title . apply_filters('widget_title', $title) . $after_title )
 					: ''
 					)
 				. $after_widget;
@@ -186,7 +186,7 @@ class fuzzy_widget extends WP_Widget {
 		echo $before_widget;
 		
 		if ( $title )
-			echo $before_title . $title . $after_title;
+			echo $before_title . apply_filters('widget_title', $title) . $after_title;
 		
 		$cur_date = false;
 		$prev_date = false;
