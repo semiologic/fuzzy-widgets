f7f727828d51058820afde4c05aed11dba26b0e1
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-12-14 14:46:20 +0000

    cache fixes
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/plugins/fuzzy-widgets/trunk@3729 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- fuzzy-widgets.php
+++ fuzzy-widgets.php
@@ -1148,18 +1148,20 @@ class fuzzy_widget extends WP_Widget {
 		if ( !$post || wp_is_post_revision($post_id) )
 			return;
 		
-		if ( wp_cache_get($post_id, 'pre_flush_post') === false )
+		$old = wp_cache_get($post_id, 'pre_flush_post');
+		if ( $old === false )
 			$old = array();
 		
 		$update = false;
 		foreach ( array(
 			'post_status',
-			) as $field => $value ) {
-			if ( !isset($o[$field]) ) {
+			) as $field ) {
+			if ( !isset($old[$field]) ) {
 				$old[$field] = $post->$field;
 				$update = true;
 			}
 		}
+		
 		
 		if ( $update )
 			wp_cache_set($post_id, $old, 'pre_flush_post');
