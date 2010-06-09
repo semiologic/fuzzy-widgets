9b169f319c8b03f8523f876aea64543ad62e730f
Denis de Bernardy <ddebarnardy@yahoo.com>
2010-06-09 13:18:55 +0000

    bump
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/plugins/fuzzy-widgets/trunk@4427 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- fuzzy-widgets.php
+++ fuzzy-widgets.php
@@ -3,7 +3,7 @@
 Plugin Name: Fuzzy Widgets
 Plugin URI: http://www.semiologic.com/software/fuzzy-widgets/
 Description: WordPress widgets that let you list recent posts, pages, links, or comments.
-Version: 3.0.5 RC
+Version: 3.0.5
 Author: Denis de Bernardy
 Author URI: http://www.getsemiologic.com
 Text Domain: fuzzy-widgets
